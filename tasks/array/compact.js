dc1646927179cd098a469667c261e8d568bcda2b
Kate <ekaterina21051988@yandex.by>
2023-06-24 19:20:56 +0300

    added compact function and test for it

---
+++ tasks/array/compact.js
@@ -0,0 +1,12 @@
+function compact(arr) {
+    let newArr = [];
+    let ind = 0
+    for(let i = 0; i < arr.length; i++){
+        if (arr[i]) {
+            newArr[ind] = arr[i];
+            ind++
+        }
+    }
+    return newArr;
+}
+module.exports = compact;
