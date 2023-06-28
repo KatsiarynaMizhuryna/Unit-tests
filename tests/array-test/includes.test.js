47d7b4ff3f1e7f5f980d734b3037ef7892a92b6b
Kate <ekaterina21051988@yandex.by>
2023-06-28 11:10:00 +0300

    fix: toEqual matcher replaced on toBe matcher in includes.test file

--- tests/array-test/includes.test.js
+++ tests/array-test/includes.test.js
@@ -1,11 +1,11 @@
 const includes = require("../../tasks/array/includes");
 
 test("Checks if value is in collection", () => {
-    expect(includes([1, 2, 3], 1, 2)).toEqual(false);
-    expect(includes([1, 2, 3], 6)).toEqual(false);
-    expect(includes([1, 2, 3], 1)).toEqual(true);
-    expect(includes({ 'a': 1, 'b': 2 }, 1)).toEqual(true);
-    expect(includes({ 'a': 1, 'b': 2 }, 8)).toEqual(false);
-    expect(includes('abcd', 'bc')).toEqual(true);
-    expect(includes('abcd', 'bca')).toEqual(false);
+    expect(includes([1, 2, 3], 1, 2)).toBe(false);
+    expect(includes([1, 2, 3], 6)).toBe(false);
+    expect(includes([1, 2, 3], 1)).toBe(true);
+    expect(includes({ 'a': 1, 'b': 2 }, 1)).toBe(true);
+    expect(includes({ 'a': 1, 'b': 2 }, 8)).toBe(false);
+    expect(includes('abcd', 'bc')).toBe(true);
+    expect(includes('abcd', 'bca')).toBe(false);
 });
